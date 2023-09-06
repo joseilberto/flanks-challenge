cbaee3b592aa33f5762d63d9bc29742024f3571f
Jose Fonceca <ilbertofjunior@gmail.com>
2023-09-06 23:39:49 +0200

    feat(service): Data classes are moved to a specific folder

---
+++ src/data_classes/__init__.py
@@ -0,0 +1,5 @@
+"""Initialise data classes"""
+
+from .data_models import ContentTypes, DataTypes, DocumentType
+
+__all__ = ["ContentTypes", "DataTypes", "DocumentType"]
---
+++ src/data_classes/data_models.py
@@ -0,0 +1,44 @@
+"""File containing all the model data classes we need"""
+
+from datetime import datetime
+from typing import Dict, List, NamedTuple, Optional, TypedDict, Union
+
+
+class ContentTypes(NamedTuple):
+    """
+    Named tuple for the content of each pagination
+    """
+
+    next_page: str = ""
+    urls: List[str] = []
+
+
+class DataTypes(NamedTuple):
+    """NamedTuple for the MongoClientBase params"""
+
+    nombre: str
+    numero_registro: str
+    fecha_registro: str
+    isin: str
+    domicilio: Optional[str] = None
+    capital_inicial: Optional[float] = None
+    capital_maximo: Optional[float] = None
+    fecha_ultimo_folleto: Optional[str] = None
+
+
+class DocumentType(TypedDict):
+    """
+    Document entry type for the database
+    """
+
+    nombre: str
+    numero_registro: str
+    fecha_registro: str
+    isin: str
+    domicilio: str
+    capital_inicial: float
+    capital_maximo: float
+    fecha_ultimo_folleto: str
+    last_update: str
+    write_date: datetime
+    updates: Dict[str, Dict[str, Union[str, float]]]
