1c46ad4c4a1dbbcfca0250d12130f215577ef9b5
Jose Fonceca <ilbertofjunior@gmail.com>
2023-09-09 11:35:01 +0200

    fix(service): Allowing the search endpoint to return only the key parameters of an entry

--- src/handlers/search_handler.py
+++ src/handlers/search_handler.py
@@ -3,16 +3,13 @@
 from typing import Dict, List, Optional, Tuple, Union
 
 from sanic.log import logger
-from sanic.response import JSONResponse, json
 from sanic.views import HTTPMethodView
 
 from config import DB_NAME
-from data_classes import QueryDict
+from data_classes import DocumentType, KeyDocumentType, QueryDict
 from mongo import DataClient
 
 from .utils import async_request_params
-
-mongo_client = DataClient(db_name=DB_NAME, collection=None, connector=None)
 
 
 class SearchHandler(HTTPMethodView):
@@ -20,7 +17,27 @@ class SearchHandler(HTTPMethodView):
     """A handler to list elements according to the search key provided"""
     log = logger
     log.info("Initialising the SearchHandler")
-    mongo_client = mongo_client
+    mongo_client = DataClient(db_name=DB_NAME, collection=None, connector=None)
+
+    async def _find_sicav_keys(self, query: QueryDict) -> List[KeyDocumentType]:
+        """
+        Find entries matching the desired query,
+        but returns only key SICAV identifiers
+        """
+        results: List[DocumentType] = await self.mongo_client.find_entries(
+            query
+        )
+        sicav_keys_results: List[KeyDocumentType] = []
+        for result in results:
+            sicav_keys_results.append(
+                {
+                    "nombre": result["nombre"],
+                    "isin": result["nombre"],
+                    "numero_registro": result["numero_registro"],
+                    "fecha_registro": result["fecha_registro"],
+                }
+            )
+        return sicav_keys_results
 
     def _format_query(self, query: QueryDict, mode: str = "$gte") -> None:
         """Format the query to unest list and create upper and lower limits"""
@@ -68,7 +85,7 @@ class SearchHandler(HTTPMethodView):
         numero_registro: Optional[Union[str, List[str]]] = None,
         nombre: Optional[str] = None,
         fecha_registro: Optional[Union[str, List[str]]] = None,
-    ) -> JSONResponse:
+    ) -> List[KeyDocumentType]:
         """Get method for searching an entry with given search parameters"""
         query_elements: List[
             Tuple[str, Optional[Union[str, List[str], Dict[str, str]]]]
@@ -83,9 +100,8 @@ class SearchHandler(HTTPMethodView):
         }
         if not query:
             self.log.error("The request received has no search parameters.")
-            return json([])
+            return []
         self._format_query(query)
         msg = f"Searching in the database using the parameters: {query}"
         self.log.info(msg)
-        results = await self.mongo_client.find_entries(query)
-        return json(results)
+        return await self._find_sicav_keys(query)
--- src/handlers/utils.py
+++ src/handlers/utils.py
@@ -3,6 +3,7 @@
 from typing import Callable, no_type_check
 
 from sanic.request import Request
+from sanic.response import JSONResponse, json
 
 
 @no_type_check
@@ -10,8 +11,8 @@ def async_request_params(fun: Callable):
     """Decorator method to collect the parameters of a request"""
 
     @no_type_check
-    async def wrapper(cls: object, request: Request):
+    async def wrapper(cls: object, request: Request) -> JSONResponse:
         """Wrapper to call the fun method using the request parameters"""
-        return await fun(cls, **request.json)
+        return json(await fun(cls, **request.json))
 
     return wrapper
